8bf52c17fada50b096df29a859f2b1909c3e54ec
KashfUlHudaa <166825328+minahil004@users.noreply.github.com>
2024-04-14 15:24:16 +0500

    First commit. currency converter app.

---
+++ index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 277
+};
+let currencyAns = await inquirer.prompt([
+    {
+        name: "from",
+        message: "enter currency you want to convert",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "Which currency you want to convert inTo",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "enter amount",
+        type: "number"
+    }
+]);
+let fromAmount = currency[currencyAns.from];
+let toAmount = currency[currencyAns.to];
+let usrAmount = currencyAns.amount;
+let baseAmount = usrAmount / fromAmount; //              15000 /280
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
+/*console.log(fromAmount);
+console.log(toAmount);
+console.log(usrAmount);*/ 
